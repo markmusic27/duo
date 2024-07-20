7c16d3cc65d92f5bfbb4b9f9568f8ffe81c2c118
Mark Music <me@markmusic.io>
2024-07-20 13:21:44 -0600

    ft => 🌵 Add ENV support to key

--- raycast/src/index.ts
+++ raycast/src/index.ts
@@ -1,13 +1,31 @@
 import { showHUD, Clipboard, LaunchProps } from "@raycast/api";
+import fetch from "node-fetch";
+import { SecretKey } from "./secret";
 
 interface CaptureProps {
-  content: string
+  content: string;
 }
 
-export default async function main(props: LaunchProps<{arguments: CaptureProps}>) {
-  console.log(props.arguments.content)
+export default async function main(props: LaunchProps<{ arguments: CaptureProps }>) {
+  //   const requestOptions = {
+  //     method: 'POST',
+  //     headers: {
+  //         'Authorization': `Bearer ${token}`,
+  //         'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //         title: 'foo',
+  //         body: 'bar',
+  //         userId: 1
+  //     })
+  // };
+  console.log(SecretKey);
 
-  const now = new Date();
-  await Clipboard.copy(now.toLocaleDateString());
-  await showHUD("Copied date to clipboard");
+  // let res = await fetch("https://jsonplaceholder.typicode.com/posts")
+
+  // console.log(res)
+
+  // const now = new Date();
+  // await Clipboard.copy(now.toLocaleDateString());
+  // await showHUD("Copied date to clipboard");
 }
