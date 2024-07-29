748c74b93c975798f9cbd8ca6998f775bfdd4d46
Mark Music <me@markmusic.io>
2024-07-28 20:50:01 -0600

    ft => 🧚🏼 update main.go

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	process "github.com/markmusic27/workspace/utils"
+	"github.com/markmusic27/workspace/handlers"
 )
 
 func main() {
@@ -17,27 +18,23 @@ func main() {
 	}
 
 	// Setup API
-	// api := gin.Default()
+	api := gin.Default()
 
-	// const port = "8080"
+	const port = "8080"
 
-	// api.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-	// api.GET("/env", handlers.VerifyEnv)
-	// api.GET("/timezone", handlers.GetTimezone)
+	api.GET("/env", handlers.VerifyEnv)
+	api.GET("/timezone", handlers.GetTimezone)
 
-	// api.POST("/sms", handlers.InboundSMSRequest)
-	// api.POST("/api", handlers.Authenticate(), handlers.InboundHTTPRequest)
-	// api.POST("/set-timezone", handlers.Authenticate(), handlers.UpdateTimezone)
+	api.POST("/sms", handlers.InboundSMSRequest)
+	api.POST("/api", handlers.Authenticate(), handlers.InboundHTTPRequest)
+	api.POST("/set-timezone", handlers.Authenticate(), handlers.UpdateTimezone)
 
-	// log.Println("Starting server on port " + port)
-	// api.Run(":" + port)
-
-	markdown := "\n  hello world    \n\n        - 123  **hello**"
-
-	process.ConvertMarkdownToNotion(markdown)
+	log.Println("Starting server on port " + port)
+	api.Run(":" + port)
 }
